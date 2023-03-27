141f401cc1e6e11d83030d7f85271ab41fdeebef
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-03-27 11:58:18 +0900

    Time: 85 ms (50.78%), Space: 45.1 MB (20.70%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var diStringMatch = function(s) {
+    const perm = [];
+    let i = 0;
+    const nums = Array.from({length: s.length+1}, ()=>i++);
+    for (let i=0; i<s.length; i++) {
+      s[i] === 'I' ? perm.push(nums.shift()) : perm.push(nums.pop());
+    }
+    perm.push(nums.pop());
+    return perm;
+};
