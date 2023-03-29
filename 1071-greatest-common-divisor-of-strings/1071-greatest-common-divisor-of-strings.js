ae04eb9452f2b8b0110d6e9f6001218c47c2ea23
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-03-29 12:57:04 +0900

    Time: 76 ms (28.41%), Space: 45.3 MB (17.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -2,6 +2,8 @@ var gcdOfStrings = function(str1, str2) {
     // 두 문자열 str1과 str2가 주어지면 x가 str1과 str2를 모두 나누도록 가장 큰 문자열 x를 반환합니다.
   let [minS, maxS] = ['', ''];
   const result = [];
+  const answer = [];
+  
   if (str1.length >= str2.length) {
     maxS = str1;
     minS = str2;
@@ -9,6 +11,7 @@ var gcdOfStrings = function(str1, str2) {
     maxS = str2;
     minS = str1;
   }
+  
   for (let i=1; i<=minS.length; i++) {
     let check = true;
     const tempMinS = minS.slice(0, i);
@@ -21,7 +24,7 @@ var gcdOfStrings = function(str1, str2) {
     }
     if (check) result.push(tempMinS);
   }
-  const answer = [];
+
   result.sort().forEach(v => {
     let tempMinS = minS;
     let tempMaxS = maxS;
@@ -33,6 +36,6 @@ var gcdOfStrings = function(str1, str2) {
     }
     if (tempMaxS === '' && tempMinS === '') answer.push(v);
   });
-  console.log(answer)
+
   return answer.length ? answer.pop() : "";
 };
