f6f66009d29c41f2827b00ebb15629ef84c81827
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-04-11 20:03:50 +0900

    Time: 86 ms (51.43%), Space: 47.2 MB (35.66%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -3,7 +3,7 @@ var maxAreaOfIsland = function(grid) {
   const WIDTH = grid[0].length;
   const HEIGHT = grid.length;
   const visited = Array.from({length: HEIGHT}, () => Array(WIDTH).fill(false));
-  let answer = [];
+  let answer = 0;
   
   const bfs = (a, b) => {
     const q = [];
@@ -13,6 +13,8 @@ var maxAreaOfIsland = function(grid) {
     
     while (q.length) {
       const [r, c] = q.pop();
+      // 큐 자료구조로 하는게 시간으로는 베스트인데 자바스크립트는 귀찮습니다. 그래서 대안으로 shift를 써도 통과됩니다. 
+      // 근데 pop써도 통과되서 시간 빠른게 기분이 좋아서 pop을 썼습니다.
       for (const move of moves) {
         const nr = r + move[0];
         const nc = c + move[1];
@@ -28,9 +30,9 @@ var maxAreaOfIsland = function(grid) {
   
   for (let i=0; i<HEIGHT; i++) {
     for (let j=0; j<WIDTH; j++) {
-      if (grid[i][j]===1 && !visited[i][j]) answer.push(bfs(i, j));
+      if (grid[i][j]===1 && !visited[i][j]) answer = Math.max(answer, bfs(i, j))
     }
   } 
 
-  return answer.length ? Math.max(...answer) : 0;
+  return answer;
 };
