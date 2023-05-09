d2d82fe19013aa9b7d23ab62bf6545c04e6d42b3
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-05-09 19:39:53 +0900

    Time: 59 ms (63.52%), Space: 42.2 MB (60.20%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  if (numRows === 1) return [[1]];
+  if (numRows === 2) return [[1],[1,1]];
+  
+  const answer = [[1],[1,1]];
+  
+  for (let i=2; i<numRows; i++) {
+    let j = 0;
+    const temp = [];
+    while (j <= i) {
+      if (j === 0 || j === i) temp.push(1);
+      else temp.push(answer[i-1][j-1] + answer[i-1][j]);
+      j++;
+    }
+    answer.push(temp);
+  }
+  
+  return answer;
+};
